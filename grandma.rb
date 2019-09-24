c524211b97dc6b7a416f5c9e693c311d596c2b14
Daniel Cowpertwait <daniel.cowpertwait@gmail.com>
2019-09-24 02:07:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 def speak_to_grandma(phrase)
+
   if phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif
-  
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "NO, NOT SINCE 1938!"
-end
+    "NO, NOT SINCE 1938!"
+  end
+
 end
